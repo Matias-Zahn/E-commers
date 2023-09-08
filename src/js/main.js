59324d748f72a8943b84f97b0e86d41b305fa1b3
Matias-Zahn <zahnmatias14@gmail.com>
2023-09-08 20:38:46 -0300

    Carrito a terminar

--- src/js/main.js
+++ src/js/main.js
@@ -35,7 +35,6 @@ function paintProducts(db){
         </div>
         `;
 
-        console.log(product);
     }
 
     productsHTML.innerHTML= html
@@ -48,29 +47,99 @@ async function main(){
 
     const db= {
         products: JSON.parse(window.localStorage.getItem('products')) || (await getElements()),
-        cart: {},
+        cart: JSON.parse(window.localStorage.getItem('cart')) ||{},
 
     };
     
     paintProducts(db)
+    cartShow()
+    iconsEvent()
+    addCarrito(db)
+    drawCarrito(db)
 }
 
 main()
 
-const cartShow= document.querySelector('.bx-menu');
-const cart= document.querySelector('.cart');
-const menu= document.querySelector('.bx-menu')
-const navbar__icons= document.querySelector('.navbar__icons')
 
-cartShow.addEventListener('click', function () {
+function cartShow() {
+    const cartShow= document.querySelector('.bx-menu');
+    const cart= document.querySelector('.cart');
 
-    if (!navbar__icons.classList.contains('bx-menu')) {
-        cart.classList.add('cart__show');
-    }
+    cartShow.addEventListener('click', () =>{
 
-    if (navbar__icons.classList.contains('bx-menu')) {
-        cart.classList.remove('cart__show')
-    }
+        cart.classList.add('cart__show')
+
+    })
+
+}
+
+
+function iconsEvent(){
+
+    const cart_icon= document.querySelector('.bxs-cart-add')
+    const carrito= document.querySelector('.carrito')
+
+    cart_icon.addEventListener('click', () => {
+        carrito.classList.toggle('carrito__show')
+    })
+}
+
+function addCarrito(db) {
     
+    const productsHTML= document.querySelector('.products')
 
-})
+    productsHTML.addEventListener('click', (e) => {
+        if (e.target.classList.contains('bx-plus')){
+
+            const id= Number(e.target.id);
+
+            const productFind= db.products.find((product) => product.id === id)
+
+            if (db.cart[productFind.id]) {
+                if(productFind.quantity === db.cart[productFind.id].amount) return alert('No tenemos mas en Stock')
+                db.cart[productFind.id].amount++;
+            } else{
+                db.cart[productFind.id] = {...productFind, amount: 1};
+            }
+
+            window.localStorage.setItem('cart', JSON.stringify(db.cart))
+        }
+
+        
+    })
+}
+
+
+function drawCarrito(db){
+
+    const carritoProducts= document.querySelector('.carrito__products')
+
+    let html= '';
+
+    for (const products in db.cart) {
+
+        const {name,quantity, price, image, id, amount} = db.cart[products]
+
+        html += `
+
+            <div class="carrito__product" id='${id}' >
+                <div class="carrito__product--img">
+                    <img src='${image}' alt='image'>
+                </div>
+                <div class='carrito__product--info'>
+                    <h5>${name}</h5>
+                    <p>Stock: ${quantity} | $${price}</p>
+                    <h4>Subtiotal: $${price} </h4>
+                    <div class="carrito__icons">
+                        <i class='bx bx-minus'></i>
+                        <p> ${amount} Unit</p>
+                        <i class='bx bx-plus'></i>
+                        <i class='bx bxs-trash' style='color:#ffffff' ></i>
+                    </div>
+                </div>
+            </div>   
+        `;
+    }
+
+    carritoProducts.innerHTML= html
+}
